0bfbc5f3aec796e16f3a4fdf801eb8249d3a29a1
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2022-11-23 18:01:53 +0600

    create login static method for User model, create controller to handle user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,22 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ name: user.name, email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup user
@@ -11,7 +25,9 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(name, email, password);
-    res.status(200).json({ email, user });
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ name, email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -19,17 +20,56 @@ const userSchema = new Schema({
   },
 });
 
-// static signup method
+//# static method for signup
 userSchema.statics.signup = async function (name, email, password) {
-  const exists = await this.findOne({ email });
+  //-> validation
+  if (!name || !email || !password) {
+    // first check every field is filled
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    // then check if the email is a valid email
+    throw Error("Email is not valid");
+  }
+
+  // if (!validator.isStrongPassword(password)) { // check if the password is strong
+  //   throw Error("Password not strong enough");
+  // }
+
+  const exists = await this.findOne({ email }); // check whether the email is already in use
   if (exists) {
     throw Error("Email already in use");
   }
 
+  //-> create and use a salt for creating more strong hash
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
+  //-> create the user in the database
   const user = await this.create({ name, email, password: hash });
+
+  return user;
+};
+
+//# static method for login
+userSchema.statics.login = async function (email, password) {
+  //-> validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  //-> check the email
+  const user = await this.findOne({ email }); // get the user if already signed up using the email
+  if (!user) {
+    throw Error("Incorrect email"); // let the user know he did not sign up with this email
+  }
+
+  //-> check the password
+  const match = await bcrypt.compare(password, user.password); // compare the provided password with the saved hash password using bcrypt
+  if (!match) {
+    throw Error("Incorrect password"); // let the user know he did not sign up with this password
+  }
 
   return user;
 };
