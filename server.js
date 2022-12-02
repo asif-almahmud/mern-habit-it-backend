9c6f56d8997feef7f88f983ea0b9809c7fc81b88
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2022-12-02 18:47:51 +0600

    allow production url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const userRoutes = require("./routes/user");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["https://habit-it.onrender.com", "http://localhost:3000"], //
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   // methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
